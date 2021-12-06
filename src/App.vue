a452c2cebf3a99f0d93023628631b3fa3fb1fe4d
ulzhanaUK <ulzhana.kylyshbek@gmail.com>
2021-12-06 12:06:00 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Hotel logo" src="./assets/1.jpg">
+    <HelloWorld msg="Welcome to Hotel chain"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
